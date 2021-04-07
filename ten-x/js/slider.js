c233c38628913f6adde80898facff6737a23a4ac
Andrew <and.roslik@gmail.com>
2021-04-08 02:33:58 +0300

    save about-box

--- ten-x/js/slider.js
+++ ten-x/js/slider.js
@@ -14,11 +14,19 @@ dot4.addEventListener("click", function() {currentSlide(4)});
 
 function currentSlide(index) {
     clearTimeout(timeoutID);
-    slideIndex = index;
-    showSlides();
+    if (index < slideIndex) {
+        let direction = "back";
+        slideIndex = index;
+        showSlides(direction);
+    } else {
+        let direction = "forward";
+        slideIndex = index;
+        showSlides(direction);
+    }
+    
 }
 
-function showSlides() {
+function showSlides(direction) {
     let i;
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -26,18 +34,18 @@ function showSlides() {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    
-    if (slideIndex < 1) {
-        slideIndex++;
-    }
-    
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
     slides[slideIndex - 1].style.display = "block";
+    if (direction == "back") {
+        slides[slideIndex - 1].style.cssText = "animation: arrive-inv .5s, leave .5s 3.5s;";
+    } else {
+        slides[slideIndex - 1].style.cssText = "animation: arrive .5s, leave .5s 3.5s;";
+    }
     dots[slideIndex - 1].className += " active";
-    slideIndex++;
-    window.timeoutID = setTimeout(showSlides, 2000);
+    //slideIndex++;
+    window.timeoutID = setTimeout(showSlides, 4000);
 }
 
 
